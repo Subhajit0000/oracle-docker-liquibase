3daa261dbed8d6f49e3dfb720502fd2f6aa2d13e
subhajitsaha <subhajitsaha@192.168.1.105>
2020-04-29 17:52:07 +0530

    Initial commit

--- scripts/table1.sql
+++ scripts/table1.sql
@@ -1,10 +1,6 @@
 --changeset subhajitsaha:1588069289387-3
 
-CREATE TABLE tab1 (
-  id          NUMBER,
-  description VARCHAR2(50),
-  CONSTRAINT tab1_pk PRIMARY KEY (id)
-);
+CREATE TABLE tab1 (id NUMBER, description VARCHAR2(50),CONSTRAINT tab1_pk PRIMARY KEY (id));
 
 INSERT INTO tab1 (id, description) VALUES (tab1_seq.NEXTVAL, 'Description for ' || tab1_seq.CURRVAL);
 COMMIT;
