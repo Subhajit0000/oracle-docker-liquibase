96688b4aa9d028afeac4d8479be926ea0b8bca00
subhajitsaha <subhajitsaha@192.168.1.105>
2020-04-29 19:06:17 +0530

    Initial commit

--- scripts/function.sql
+++ scripts/function.sql
@@ -1,11 +1,12 @@
 --changeset subhajitsaha:1588069289387-1
+--changeset subhajitsaha:1588069289387-8
 
 CREATE OR REPLACE FUNCTION get_tab1_count RETURN NUMBER AS
   l_count  NUMBER;
 BEGIN
   SELECT COUNT(*)
   INTO   l_count
-  FROM   tab1;
+  FROM   tab2;
 
   RETURN l_count;
 END;
